cf49b39500f68f17562505fd12c9ad8fd600f7f6
cssangeroki <csangeroki@gmail.com>
2025-03-20 01:15:20 -0700

    Fixed Z-index

--- V2Code/personal-website-v2/src/app/components/RetroFilter/retro-filter.index.tsx
+++ V2Code/personal-website-v2/src/app/components/RetroFilter/retro-filter.index.tsx
@@ -11,6 +11,9 @@ const RetroFilter: React.FC<ImageComponentProps> = ({ imageUrl, altText }) => {
             <div className="size blend blend-yellow vignette">
                 <img src={imageUrl} alt={altText} className="size"/>
             </div>
+            <div className="size">
+                <img src={imageUrl} alt={altText} className="size"/>
+            </div>
         </>
     );
 };
